3cb02cefffd4c76a01b394e07af3b7f4a91b2cc4
Attila-Mihály Balázs <dify.ltd@gmail.com>
2015-07-06 16:40:46 +0300

    Add a version of MappedStore which can be resized
    The file-handle is kept open during the resize which means that it will work correcly even on "anonymous" files (files which have been deleted from the filesystem).
    
    Warning! Access the Bytes from before the resize call after resize have been called can have undefined consequences (including a JVM crash) and it's the callers responsibility to ensure that that never happens.

--- lang/src/main/java/net/openhft/lang/io/ResizableMappedStore.java
+++ lang/src/main/java/net/openhft/lang/io/ResizableMappedStore.java
@@ -13,51 +13,50 @@
  *     You should have received a copy of the GNU Lesser General Public License
  *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package net.openhft.lang.io;
-
-import net.openhft.lang.io.serialization.BytesMarshallableSerializer;
-import net.openhft.lang.io.serialization.BytesMarshallerFactory;
-import net.openhft.lang.io.serialization.JDKZObjectSerializer;
-import net.openhft.lang.io.serialization.ObjectSerializer;
-import net.openhft.lang.io.serialization.impl.VanillaBytesMarshallerFactory;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
 
-public class MappedStore extends AbstractMappedStore {
-    public MappedStore(File file, FileChannel.MapMode mode, long size) throws IOException {
-        this(file, mode, size, new VanillaBytesMarshallerFactory());
+import net.openhft.lang.io.serialization.BytesMarshallableSerializer;
+import net.openhft.lang.io.serialization.JDKZObjectSerializer;
+import net.openhft.lang.io.serialization.ObjectSerializer;
+import net.openhft.lang.io.serialization.impl.VanillaBytesMarshallerFactory;
+
+public final class ResizableMappedStore extends AbstractMappedStore {
+    public ResizableMappedStore(File file, FileChannel.MapMode mode, long size) throws IOException {
+        this(file, mode, size, BytesMarshallableSerializer.create(new VanillaBytesMarshallerFactory(), JDKZObjectSerializer.INSTANCE));
     }
 
-    @Deprecated
-    public MappedStore(File file, FileChannel.MapMode mode, long size, BytesMarshallerFactory bytesMarshallerFactory) throws IOException {
-        this(file, mode, size, BytesMarshallableSerializer.create(bytesMarshallerFactory, JDKZObjectSerializer.INSTANCE));
+    public ResizableMappedStore(File file, FileChannel.MapMode mode, long size, ObjectSerializer objectSerializer) throws IOException {
+        super(new ReadWriteMmapInfoHolder(), file, mode, size, objectSerializer);
     }
 
-    public MappedStore(File file, FileChannel.MapMode mode, long size, ObjectSerializer objectSerializer) throws IOException {
-        super(new ReadOnlyMmapInfoHolder(), file, mode, size, objectSerializer);
-        ((ReadOnlyMmapInfoHolder)mmapInfoHolder).lock();
+
+    /**
+     * Resizes the underlying file and re-maps it. Warning! After this call
+     * instances of {@link Bytes} obtained through {@link #bytes()} or
+     * {@link #bytes(long, long)} are invalid and using them can lead to reading
+     * arbitrary data or JVM crash! It is the callers responsibility to ensure
+     * that these instances are not used after the method call.
+     * 
+     * @param newSize
+     * @throws IOException
+     */
+    public void resize(long newSize) throws IOException {
+        validateSize(newSize);
+        unmapAndSyncToDisk();
+        resizeIfNeeded(newSize);
+        this.mmapInfoHolder.setSize(newSize);
+        map();
     }
 
-    private static final class ReadOnlyMmapInfoHolder extends MmapInfoHolder {
+    private static final class ReadWriteMmapInfoHolder extends MmapInfoHolder {
         private long address, size;
-        private boolean locked;
-
-        private void checkLock() {
-            if (locked) {
-                throw new IllegalStateException();
-            }
-        }
-
-        void lock() {
-            this.locked = true;
-        }
 
         @Override
         void setAddress(long address) {
-            checkLock();
             this.address = address;
         }
 
@@ -68,7 +67,6 @@ public class MappedStore extends AbstractMappedStore {
 
         @Override
         void setSize(long size) {
-            checkLock();
             this.size = size;
         }
 
@@ -78,4 +76,3 @@ public class MappedStore extends AbstractMappedStore {
         }
     }
 }
-
