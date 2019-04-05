624f33d941cca6913210ccad2aa141812ad7941a
rupinder10 <rupinder10@gmail.com>
2019-04-05 12:59:21 -0400

    Reformatted using standard java formatting

--- lang/src/main/java/net/openhft/lang/io/Bits.java
+++ lang/src/main/java/net/openhft/lang/io/Bits.java
@@ -1,170 +1,174 @@
 package net.openhft.lang.io;
 
+import java.lang.reflect.Field;
+import java.nio.ByteOrder;
+
 import org.jetbrains.annotations.NotNull;
 import sun.misc.Unsafe;
 
 @SuppressWarnings("restriction")
 public class Bits {
 
-  @NotNull
-  protected static final Unsafe UNSAFE;
+	@NotNull
+	protected static final Unsafe UNSAFE;
 
-  public static final String MEMORY_ALIGNED_ACCESS_PROPERTY = "net.openhft.io.memory.aligned.access";
-  public static final String BYTE_ORDER_BIGENDIAN_PROPERTY = "net.openhft.io.byte.order.bigEndian";
+	public static final String MEMORY_ALIGNED_ACCESS_PROPERTY = "net.openhft.io.memory.aligned.access";
+	public static final String BYTE_ORDER_BIGENDIAN_PROPERTY = "net.openhft.io.byte.order.bigEndian";
 
-  public static final ByteOrder NATIVE_BYTE_ORDER = ByteOrder.nativeOrder();
+	public static final ByteOrder NATIVE_BYTE_ORDER = ByteOrder.nativeOrder();
 
-  /** Unaligned flag. */
-  private static final Bits ALIGNED = aligned();
+	/** Unaligned flag. */
+	private static final Bits ALIGNED = aligned();
 
-  static {
-    try {
-      Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
-      theUnsafe.setAccessible(true);
-      UNSAFE = (Unsafe) theUnsafe.get(null);
-    } catch (Exception e) {
-      throw new AssertionError(e);
-    }
+	static {
+		try {
+			Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
+			theUnsafe.setAccessible(true);
+			UNSAFE = (Unsafe) theUnsafe.get(null);
+		} catch (Exception e) {
+			throw new AssertionError(e);
+		}
+	}
 
-  public static short getShort(long addr) {
-    return ALIGNED.getShortByByte(addr);
-  }
+	public static short getShort(long addr) {
+		return ALIGNED.getShortByByte(addr);
+	}
 
-  public static void putShort(long addr, short v) {
-    ALIGNED.putShortByByte(addr, v);
-  }
+	public static void putShort(long addr, short v) {
+		ALIGNED.putShortByByte(addr, v);
+	}
 
-  public static char getChar(long addr) {
-    return ALIGNED.getCharByByte(addr);
-  }
+	public static char getChar(long addr) {
+		return ALIGNED.getCharByByte(addr);
+	}
 
-  public static void putChar(long addr, char v) {
-    ALIGNED.putCharByByte(addr, (char) v);
-  }
+	public static void putChar(long addr, char v) {
+		ALIGNED.putCharByByte(addr, (char) v);
+	}
 
-  public static int getInt(long addr) {
-    return ALIGNED.getIntByByte(addr);
-  }
+	public static int getInt(long addr) {
+		return ALIGNED.getIntByByte(addr);
+	}
 
-  public static void putInt(long addr, int v) {
-    ALIGNED.putIntByByte(addr, v);
-  }
+	public static void putInt(long addr, int v) {
+		ALIGNED.putIntByByte(addr, v);
+	}
 
-  public static long getLong(long addr) {
-    return ALIGNED.getLongByByte(addr);
-  }
+	public static long getLong(long addr) {
+		return ALIGNED.getLongByByte(addr);
+	}
 
-  public static void putLong(long addr, long v) {
-    ALIGNED.putLongByByte(addr, v);
-  }
+	public static void putLong(long addr, long v) {
+		ALIGNED.putLongByByte(addr, v);
+	}
 
-  public static float getFloat(long addr) {
-    return Float.intBitsToFloat(ALIGNED.getIntByByte(addr));
-  }
+	public static float getFloat(long addr) {
+		return Float.intBitsToFloat(ALIGNED.getIntByByte(addr));
+	}
 
-  public static void putFloat(long addr, float v) {
-    ALIGNED.putIntByByte(addr, Float.floatToIntBits(v));
-  }
+	public static void putFloat(long addr, float v) {
+		ALIGNED.putIntByByte(addr, Float.floatToIntBits(v));
+	}
 
-  public static double getDouble(long addr) {
-    return Double.longBitsToDouble(ALIGNED.getLongByByte(addr));
-  }
+	public static double getDouble(long addr) {
+		return Double.longBitsToDouble(ALIGNED.getLongByByte(addr));
+	}
 
-  public static void putDouble(long addr, double v) {
-    ALIGNED.putLongByByte(addr, Double.doubleToLongBits(v));
-  }
+	public static void putDouble(long addr, double v) {
+		ALIGNED.putLongByByte(addr, Double.doubleToLongBits(v));
+	}
 
-  /**
-   * @param addr
-   *          Address.
-   */
-  protected short getShortByByte(long addr) {
-    return UNSAFE.getShort(addr);
-  }
+	/**
+	 * @param addr
+	 *            Address.
+	 */
+	protected short getShortByByte(long addr) {
+		return UNSAFE.getShort(addr);
+	}
 
-  /**
-   * @param addr
-   *          Address.
-   * @param val
-   *          Value.
-   */
-  protected void putShortByByte(long addr, short val) {
-    UNSAFE.putShort(addr, val);
-  }
+	/**
+	 * @param addr
+	 *            Address.
+	 * @param val
+	 *            Value.
+	 */
+	protected void putShortByByte(long addr, short val) {
+		UNSAFE.putShort(addr, val);
+	}
 
-  /**
-   * @param addr
-   *          Address.
-   */
-  protected char getCharByByte(long addr) {
-    return UNSAFE.getChar(addr);
-  }
+	/**
+	 * @param addr
+	 *            Address.
+	 */
+	protected char getCharByByte(long addr) {
+		return UNSAFE.getChar(addr);
+	}
 
-  /**
-   * @param addr
-   *          Address.
-   * @param val
-   *          Value.
-   */
-  protected void putCharByByte(long addr, char val) {
-    UNSAFE.putChar(addr, (char) val);
-  }
+	/**
+	 * @param addr
+	 *            Address.
+	 * @param val
+	 *            Value.
+	 */
+	protected void putCharByByte(long addr, char val) {
+		UNSAFE.putChar(addr, (char) val);
+	}
 
-  /**
-   * @param addr
-   *          Address.
-   */
-  protected int getIntByByte(long addr) {
-    return UNSAFE.getInt(addr);
-  }
+	/**
+	 * @param addr
+	 *            Address.
+	 */
+	protected int getIntByByte(long addr) {
+		return UNSAFE.getInt(addr);
+	}
 
-  /**
-   * @param addr
-   *          Address.
-   * @param val
-   *          Value.
-   */
-  protected void putIntByByte(long addr, int val) {
-    UNSAFE.putInt(addr, val);
-  }
+	/**
+	 * @param addr
+	 *            Address.
+	 * @param val
+	 *            Value.
+	 */
+	protected void putIntByByte(long addr, int val) {
+		UNSAFE.putInt(addr, val);
+	}
 
-  /**
-   * @param addr
-   *          Address.
-   */
-  protected long getLongByByte(long addr) {
-    return UNSAFE.getLong(addr);
-  }
+	/**
+	 * @param addr
+	 *            Address.
+	 */
+	protected long getLongByByte(long addr) {
+		return UNSAFE.getLong(addr);
+	}
 
-  /**
-   * @param addr
-   *          Address.
-   * @param val
-   *          Value.
-   */
-  protected void putLongByByte(long addr, long val) {
-    UNSAFE.putLong(addr, val);
-  }
+	/**
+	 * @param addr
+	 *            Address.
+	 * @param val
+	 *            Value.
+	 */
+	protected void putLongByByte(long addr, long val) {
+		UNSAFE.putLong(addr, val);
+	}
 
-  /**
-   * Returns unaligned flag.
-   */
-  private static Bits aligned() {
-    String arch = System.getProperty("os.arch");
-    String model = System.getProperty("sun.arch.data.model");
-    boolean aligned = (arch.equals("sparcv9") || arch.equals("sparc")) && model.equals("64");
-    if (!aligned)
-      aligned = Boolean.getBoolean(MEMORY_ALIGNED_ACCESS_PROPERTY);
-    if (aligned) {
-      return bigEndian() ? new BitsAlignedBigEndian() : new BitsAligned();
-    }
-    return new Bits();
-  }
+	/**
+	 * Returns unaligned flag.
+	 */
+	private static Bits aligned() {
+		String arch = System.getProperty("os.arch");
+		String model = System.getProperty("sun.arch.data.model");
+		boolean aligned = (arch.equals("sparcv9") || arch.equals("sparc")) && model.equals("64");
+		if (!aligned)
+			aligned = Boolean.getBoolean(MEMORY_ALIGNED_ACCESS_PROPERTY);
+		if (aligned) {
+			return bigEndian() ? new BitsAlignedBigEndian() : new BitsAligned();
+		}
+		return new Bits();
+	}
 
-  private static boolean bigEndian() {
-    if (System.getProperty(BYTE_ORDER_BIGENDIAN_PROPERTY) == null) {
-      return ByteOrder.nativeOrder() == ByteOrder.BIG_ENDIAN;
-    }
-    return Boolean.getBoolean(BYTE_ORDER_BIGENDIAN_PROPERTY);
-  }
+	private static boolean bigEndian() {
+		if (System.getProperty(BYTE_ORDER_BIGENDIAN_PROPERTY) == null) {
+			return ByteOrder.nativeOrder() == ByteOrder.BIG_ENDIAN;
+		}
+		return Boolean.getBoolean(BYTE_ORDER_BIGENDIAN_PROPERTY);
+	}
 }
--- lang/src/main/java/net/openhft/lang/io/BitsAligned.java
+++ lang/src/main/java/net/openhft/lang/io/BitsAligned.java
@@ -3,59 +3,59 @@ package net.openhft.lang.io;
 @SuppressWarnings("restriction")
 public class BitsAligned extends Bits {
 
-  @Override
-  protected short getShortByByte(long addr) {
-    return (short) (UNSAFE.getByte(addr + 1) << 8 | (UNSAFE.getByte(addr) & 0xff));
-  }
+	@Override
+	protected short getShortByByte(long addr) {
+		return (short) (UNSAFE.getByte(addr + 1) << 8 | (UNSAFE.getByte(addr) & 0xff));
+	}
 
-  @Override
-  protected void putShortByByte(long addr, short val) {
-    UNSAFE.putByte(addr + 1, (byte) (val >> 8));
-    UNSAFE.putByte(addr, (byte) val);
-  }
+	@Override
+	protected void putShortByByte(long addr, short val) {
+		UNSAFE.putByte(addr + 1, (byte) (val >> 8));
+		UNSAFE.putByte(addr, (byte) val);
+	}
 
-  @Override
-  protected char getCharByByte(long addr) {
-    return (char) (UNSAFE.getByte(addr + 1) << 8 | (UNSAFE.getByte(addr) & 0xff));
-  }
+	@Override
+	protected char getCharByByte(long addr) {
+		return (char) (UNSAFE.getByte(addr + 1) << 8 | (UNSAFE.getByte(addr) & 0xff));
+	}
 
-  @Override
-  protected void putCharByByte(long addr, char val) {
-    UNSAFE.putByte(addr + 1, (byte) (val >> 8));
-    UNSAFE.putByte(addr, (byte) val);
-  }
+	@Override
+	protected void putCharByByte(long addr, char val) {
+		UNSAFE.putByte(addr + 1, (byte) (val >> 8));
+		UNSAFE.putByte(addr, (byte) val);
+	}
 
-  @Override
-  protected int getIntByByte(long addr) {
-    return (((int) UNSAFE.getByte(addr + 3)) << 24) | (((int) UNSAFE.getByte(addr + 2) & 0xff) << 16)
-        | (((int) UNSAFE.getByte(addr + 1) & 0xff) << 8) | (((int) UNSAFE.getByte(addr) & 0xff));
-  }
+	@Override
+	protected int getIntByByte(long addr) {
+		return (((int) UNSAFE.getByte(addr + 3)) << 24) | (((int) UNSAFE.getByte(addr + 2) & 0xff) << 16)
+				| (((int) UNSAFE.getByte(addr + 1) & 0xff) << 8) | (((int) UNSAFE.getByte(addr) & 0xff));
+	}
 
-  @Override
-  protected void putIntByByte(long addr, int val) {
-    UNSAFE.putByte(addr + 3, (byte) (val >> 24));
-    UNSAFE.putByte(addr + 2, (byte) (val >> 16));
-    UNSAFE.putByte(addr + 1, (byte) (val >> 8));
-    UNSAFE.putByte(addr, (byte) (val));
-  }
+	@Override
+	protected void putIntByByte(long addr, int val) {
+		UNSAFE.putByte(addr + 3, (byte) (val >> 24));
+		UNSAFE.putByte(addr + 2, (byte) (val >> 16));
+		UNSAFE.putByte(addr + 1, (byte) (val >> 8));
+		UNSAFE.putByte(addr, (byte) (val));
+	}
 
-  @Override
-  protected long getLongByByte(long addr) {
-    return (((long) UNSAFE.getByte(addr + 7)) << 56) | (((long) UNSAFE.getByte(addr + 6) & 0xff) << 48)
-        | (((long) UNSAFE.getByte(addr + 5) & 0xff) << 40) | (((long) UNSAFE.getByte(addr + 4) & 0xff) << 32)
-        | (((long) UNSAFE.getByte(addr + 3) & 0xff) << 24) | (((long) UNSAFE.getByte(addr + 2) & 0xff) << 16)
-        | (((long) UNSAFE.getByte(addr + 1) & 0xff) << 8) | (((long) UNSAFE.getByte(addr) & 0xff));
-  }
+	@Override
+	protected long getLongByByte(long addr) {
+		return (((long) UNSAFE.getByte(addr + 7)) << 56) | (((long) UNSAFE.getByte(addr + 6) & 0xff) << 48)
+				| (((long) UNSAFE.getByte(addr + 5) & 0xff) << 40) | (((long) UNSAFE.getByte(addr + 4) & 0xff) << 32)
+				| (((long) UNSAFE.getByte(addr + 3) & 0xff) << 24) | (((long) UNSAFE.getByte(addr + 2) & 0xff) << 16)
+				| (((long) UNSAFE.getByte(addr + 1) & 0xff) << 8) | (((long) UNSAFE.getByte(addr) & 0xff));
+	}
 
-  @Override
-  protected void putLongByByte(long addr, long val) {
-    UNSAFE.putByte(addr + 7, (byte) (val >> 56));
-    UNSAFE.putByte(addr + 6, (byte) (val >> 48));
-    UNSAFE.putByte(addr + 5, (byte) (val >> 40));
-    UNSAFE.putByte(addr + 4, (byte) (val >> 32));
-    UNSAFE.putByte(addr + 3, (byte) (val >> 24));
-    UNSAFE.putByte(addr + 2, (byte) (val >> 16));
-    UNSAFE.putByte(addr + 1, (byte) (val >> 8));
-    UNSAFE.putByte(addr, (byte) (val));
-  }
+	@Override
+	protected void putLongByByte(long addr, long val) {
+		UNSAFE.putByte(addr + 7, (byte) (val >> 56));
+		UNSAFE.putByte(addr + 6, (byte) (val >> 48));
+		UNSAFE.putByte(addr + 5, (byte) (val >> 40));
+		UNSAFE.putByte(addr + 4, (byte) (val >> 32));
+		UNSAFE.putByte(addr + 3, (byte) (val >> 24));
+		UNSAFE.putByte(addr + 2, (byte) (val >> 16));
+		UNSAFE.putByte(addr + 1, (byte) (val >> 8));
+		UNSAFE.putByte(addr, (byte) (val));
+	}
 }
